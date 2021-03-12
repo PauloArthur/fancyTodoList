770266ca4eaab9e3ffb6be11b90b527eda3a1d79
PauloArthur <pauloarthurcostamendes@gmail.com>
2021-03-12 00:02:47 -0300

    Adding SignIn and SignUp Content

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Text, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+
 import { Container } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <Text>SignIn</Text>
+      <Button onPress={() => navigation.navigate("SignUp")} title="SignUp"/>
+    </Container>
+  )
 }
 
 export default SignIn;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 import { Container } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <Text>SignUp</Text>
+      <Button onPress={() => navigation.goBack()} title="Back"/>
+    </Container>
+  )
 }
 
 export default SignUp;
